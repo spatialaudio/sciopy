5386e24db346ef3df1a18570462aa5e8216d67ba
JacobTh98 <62538704+JacobTh98@users.noreply.github.com>
2023-02-08 08:59:13 +0100

    Merge dev branch into main (#1)
    * uploaded versuin==0.2
    
    * updated README.md
    
    * fixed classes for v==0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sciopy",
-    version="0.1",
+    version="0.2.1",
     author="Jacob Peter Thönes",
     author_email="jacob.thoenes@uni-rostock.de",
     description="Python based interface module for serial communication with the ScioSpec Electrical Impedance Tomography (EIT) device.",
@@ -11,4 +11,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    url="https://github.com/spatialaudio/sciopy.git",
 )
