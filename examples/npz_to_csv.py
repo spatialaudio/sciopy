dfce6c7bf7f944a4bd8c413181ef443598325373
JacobTh98 <jacob.thoenes@googlemail.com>
2023-03-10 13:34:10 +0100

    linting

--- examples/npz_to_csv.py
+++ examples/npz_to_csv.py
@@ -1,6 +1,6 @@
 from sciopy.npztocsv import s_dict_n_el_16
 from sciopy import convert_measurement_directory_n_el_16_r_split
 
-lpath = "../../ScioSpecMeasurements/obj_d_10/" # path to measurement directory
+lpath = "../../ScioSpecMeasurements/obj_d_10/"  # path to measurement directory
 
 convert_measurement_directory_n_el_16_r_split(lpath, s_dict_n_el_16)
