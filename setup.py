2308d6b35aa3f8954383a4d74592ce76adae6537
jt292 <jacob.thoenes@uni-rostock.de>
2023-03-13 18:01:34 +0100

    v==0.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="sciopy",
-    version="0.6.0",
+    version="0.6.1",
     author="Jacob Peter Thönes",
     author_email="jacob.thoenes@uni-rostock.de",
     description="Python based interface module for serial communication with the ScioSpec Electrical Impedance Tomography (EIT) device.",
