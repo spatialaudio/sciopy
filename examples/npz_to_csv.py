90edf9c3b1d84475867960e52b30a226f0432863
JacobTh98 <jacob.thoenes@uni-rostock.de>
2023-05-16 13:33:24 +0200

    fixed npz to csv conversion error

--- examples/npz_to_csv.py
+++ examples/npz_to_csv.py
@@ -1,6 +1,14 @@
 from sciopy.npztocsv import s_dict_n_el_16
-from sciopy import convert_measurement_directory_n_el_16_r_split
+from sciopy.sciopy_dataclasses import PreperationConfig
+from sciopy import prepare_all_samples_for_16_el_single
 
-lpath = "../../ScioSpecMeasurements/obj_d_10/"  # path to measurement directory
+path = "obj_d_10"
 
-convert_measurement_directory_n_el_16_r_split(lpath, s_dict_n_el_16)
+prep_conf = PreperationConfig(lpath=path, spath=path, n_samples=len(os.listdir(path)))
+
+prepare_all_samples_for_16_el_single(prep_conf)
+
+
+# Only working with 3D printer object movement and class "enderstat"
+# from sciopy import convert_measurement_directory_n_el_16_r_split
+# convert_measurement_directory_n_el_16_r_split(lpath, s_dict_n_el_16)
