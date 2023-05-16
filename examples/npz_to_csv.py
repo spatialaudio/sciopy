fbf8cf6f40e56d95acc7b26ace86ac7a266a54f6
jt292 <jacob.thoenes@uni-rostock.de>
2023-05-16 14:00:06 +0200

    changed names of example measurements

--- examples/npz_to_csv.py
+++ examples/npz_to_csv.py
@@ -1,14 +1,9 @@
-from sciopy.npztocsv import s_dict_n_el_16
 from sciopy.sciopy_dataclasses import PreperationConfig
 from sciopy import prepare_all_samples_for_16_el_single
+import os
 
 path = "obj_d_10"
 
 prep_conf = PreperationConfig(lpath=path, spath=path, n_samples=len(os.listdir(path)))
 
 prepare_all_samples_for_16_el_single(prep_conf)
-
-
-# Only working with 3D printer object movement and class "enderstat"
-# from sciopy import convert_measurement_directory_n_el_16_r_split
-# convert_measurement_directory_n_el_16_r_split(lpath, s_dict_n_el_16)
